04c68d2dc538a532fbd22e65f250f7182c43ee2b
Ramya0694 <ramya0694@gmail.com>
2021-05-14 16:03:57 -0700

    Join model

--- models/example/race_symptoms.sql
+++ models/example/race_symptoms.sql
@@ -1,12 +1,14 @@
 {{ config(materialized='table') }}
 
-SELECT state, sub_region_2, case_month, race, race_cases, symptom, sym_weight  FROM
+SELECT state, sub_region_2 AS county, case_month AS month , race, race_cases, symptom, sym_weight  FROM
 (SELECT state, location, race, AVG(value) race_cases, SUBSTRING(CAST(date AS string), 0, 7) case_month
 FROM {{ ref('cases_unpivot') }} cases
+WHERE value > 0
 GROUP BY case_month, state, location, race) AS temp1
-LEFT OUTER JOIN
+INNER JOIN
 (SELECT sub_region_1_code, sub_region_2, symptom, AVG(value) sym_weight, SUBSTRING(CAST(date AS string), 0, 7) sym_month
 FROM `{{ ref('symptoms_unpivot_cleaned') }} symptoms
+WHERE value > 0
 GROUP BY sym_month, sub_region_1_code, sub_region_2, symptom) AS temp2
 ON sym_month = case_month
 AND temp2.sub_region_2 = temp1.location
