da9e9245abd40733943ce4cdbcae336627de3887
Ramya0694 <ramya0694@gmail.com>
2021-05-14 18:16:56 -0700

    Rank model

--- models/example/top_race_symptoms.sql
+++ models/example/top_race_symptoms.sql
@@ -4,4 +4,4 @@ SELECT *
 FROM (SELECT month, state, county, race, symptom, RANK() OVER(PARTITION BY month, state, county,race ORDER BY sym_weight DESC) AS rank
 FROM {{ ref('race_symptoms') }}
 ORDER BY rank) AS sub
-WHERE rank < 4
+WHERE rank < 10
