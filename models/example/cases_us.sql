ec1d489f33a892084e57d1c196fc34ad98de97eb
sehgalvibhor <sehgalvibhor@berkeley.edu>
2021-05-15 05:43:39 +0000

    Added tests

--- models/example/cases_us.sql
+++ models/example/cases_us.sql
@@ -3,15 +3,10 @@
         materialized='incremental',
     )
 }}
-
 SELECT *
 FROM {{ source('cov_track', 'city_level_cases_and_deaths') }}
-
+WHERE city_or_county = 'County'
 {% if is_incremental() %}
-
   -- this filter will only be applied on an incremental run
-  where date <= (SELECT CURRENT_DATE AS today)
-
+  AND date <= (SELECT CURRENT_DATE AS today)
 {% endif %}
-
-AND city_or_county = 'County'
