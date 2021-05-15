5e13ac51e1a36fc0ab8c27c4efb10f6829b64538
Ramya0694 <ramya0694@gmail.com>
2021-05-14 19:50:37 -0700

    Inc model

--- models/example/cases_us.sql
+++ models/example/cases_us.sql
@@ -1,4 +1,16 @@
-{{ config(materialized='table') }}
+{{
+    config(
+        materialized='incremental',
+    )
+}}
 
-SELECT * FROM `bigquery-public-data.covid19_tracking.city_level_cases_and_deaths`
+SELECT *
+FROM `bigquery-public-data.covid19_tracking.city_level_cases_and_deaths`
 WHERE city_or_county = 'County'
+
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  where date <= (SELECT CURRENT_DATE AS today)
+
+{% endif %}
