c848466b6a9546fe99016a9fc790c978e5e91612
Ramya0694 <ramya0694@gmail.com>
2021-05-14 19:30:25 -0700

    Mask model

--- models/example/race_symptom_mask_usage.sql
+++ models/example/race_symptom_mask_usage.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='table') }}
 
 SELECT map.state, map.county, month, race, first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth, never, rarely, sometimes, frequently, always
-FROM {{ ref('top_covid_symptoms_by_race') }} covid, { ref('county_code_mapping') }} map,
+FROM {{ ref('top_covid_symptoms_by_race') }} covid, {{ ref('county_code_mapping') }} map,
 `bigquery-public-data.covid19_nyt.mask_use_by_county` masks
 WHERE covid.county = map.county
 AND map.county_code = masks.county_fips_code
