15fabb2f18abdc4515605b695d5e753eb93c209c
sehgalvibhor <sehgalvibhor@berkeley.edu>
2021-05-15 02:00:13 +0000

    new table

---
+++ models/example/covid_symptoms_by_rank_and_race.sql
@@ -0,0 +1,15 @@
+{{ config(materialized='table') }}
+
+SELECT * from  {{ ref('top_race_symptoms') }}
+pivot ( 
+  sum(rank) for symptom in ('symptom_Pain' symptom_Pain, 
+'symptom_Infection' symptom_Infection, 
+'symptom_Allergy' symptom_Allergy, 
+'symptom_Acne' symptom_Acne, 
+'symptom_Anxiety' symptom_Anxiety, 
+'symptom_Diabetes' symptom_Diabetes, 
+'symptom_Itch' symptom_Itch, 
+'symptom_Skin_rash' symptom_Skin_rash, 
+'symptom_Common_cold' symptom_Common_cold, 
+'symptom_Hypertension' symptom_Hypertension)
+);
